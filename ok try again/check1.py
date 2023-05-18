0ec94cf237bbcb0113d9f91bce03d84ec7565b47
palakmrig <palak.m1608@gmail.com>
2023-05-18 15:48:43 +0530

    now try

---
+++ ok try again/check1.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# boolean(True_1/False_0), int (integer), float(decimal), complex,  string(characters/textual)
+
+# In[55]:
+
+
+x = True
+type(x)
+
+
+# I'm assigning 12 to y 
+
+# In[37]:
+
+
+'''I'm Assigning 12 to y
+vskjdbvdai
+kjsdvba'''
+y = 12
+type(y)
+
+
+# In[3]:
+
+
+"""dfjvbsdfkjbds
+kjsdbvkvblads
+nvkxjvk
+bsdjdbvso"""
+
+z = -2.45
+type(z)
+
+
+# In[38]:
+
+
+a = 2+3j
+type(a)
+
+
+# In[39]:
+
+
+b = "Sachin /,. "
+type(b)
+
+
+# In[40]:
+
+
+print(b)
+
+
+# In[41]:
+
+
+"Hello World"
+
+
+# In[7]:
+
+
+print(b)
+
+
+# In[8]:
+
+
+print(x + y)
+print(x - y)
+"Hellow World"
+
+
+# In[42]:
+
+
+z = x + y
+c = x - y
+type(c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+type(x)
+
+
+# In[51]:
+
+
+print(r"Sachin \\\\\ 'ndvfs' ") # r means raw. will print all three backslashes.
+# bina r ke backslash
+
+
+# In[58]:
+
+
+x = input("Enter User ID:")
+print(x)
+type(x)
+
+
+# In[59]:
+
+
+print(x)
+
+
+# In[60]:
+
+
+type(x)
+
+
+# In[78]:
+
+
+print("7")
+print(type("7"))
+print(type(int("7")))
+
+
+# In[ ]:
+
+
+
+
+
+# In[79]:
+
+
+# y =   int(input("Enter any integer: "))
+
+y = input("Enter any integer: ")  # by default the entry will be string
+z = int(float(y))
+print(z)
+type(z)
+
+
+# In[74]:
+
+
+type(y)
+
+
+# In[75]:
+
+
+float(y)
+
+
+# In[81]:
+
+
+"2.5" + "5.6"
+
+
+# In[25]:
+
+
+2.5+5.6
+
+
+# In[29]:
+
+
+y = input("Enter any integer: ")
+int(y)
+
+
+# In[30]:
+
+
+type(y)
+
+
+# In[31]:
+
+
+z = int(y)
+
+
+# In[32]:
+
+
+type(z)
+
+
+# In[83]:
+
+
+type(y)
+type(z)
+
+
+# In[84]:
+
+
+print(type(y))
+type(z)
+
+
+# In[85]:
+
+
+type(y)
+print(type(z))
+
+
+# In[86]:
+
+
+print(type(y))
+print(type(z))
+
+
+# "My name is Sachin."
+# "My name is"   "Sachin"
+
+# In[35]:
+
+
+print("My name is "+str(23))
+
+
+# In[90]:
+
+
+x = "Palak Mrig"
+print("My name is",x)
+
+
+# In[92]:
+
+
+x = "Palak Mrig"
+print("My name is",x+".")
+
+
+# In[94]:
+
+
+x = "Palak Mrig"
+print("My name is "+x+".")
+
+
+# In[95]:
+
+
+# String Formatting
+print(f"My name is {x}.")
+
+
+# In[ ]:
+
+
+
+
